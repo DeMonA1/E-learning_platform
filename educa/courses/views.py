6f7ac23c4333f83a4ace96bcb586f636dbc2bc81
DeMonA1 <demonagrail1@gmail.com>
2024-11-15 13:19:04 +0300

    views to change order of contents and modules(d&d)

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -6,6 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
@@ -186,3 +187,28 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+    
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, 
+                      View):
+    """
+    Change the order of modules via drag and  drop
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, 
+                       View):
+    """
+    Change the order of contents via drag and  drop
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user
+                                    ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
