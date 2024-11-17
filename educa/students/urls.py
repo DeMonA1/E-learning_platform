9c9f762048ee22e663b0d29a9c2607c43f42ec20
DeMonA1 <demonagrail1@gmail.com>
2024-11-17 09:06:44 +0300

    the view+URL for users courses enrollment system

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.StudentRegistrationView.as_view(),
          name='student_registration'),
+    path('enroll-course/', views.StudentEnrollCourseView.as_view(),
+         name='student_enroll_course'),
 ]
