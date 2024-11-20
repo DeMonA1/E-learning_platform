806713f90ce1114aa0ae931f2105f60b9fa928a9
DeMonA1 <demonagrail1@gmail.com>
2024-11-20 19:04:24 +0300

    SubjectViewSet

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -8,12 +8,14 @@ app_name = 'courses'
 
 router = routers.DefaultRouter()
 router.register('courses', views.CourseViewSet)
+router.register('subjects', views.SubjectViewSet)
 
 urlpatterns = [
-     path('subjects/', views.SubjectListView.as_view(), 
-          name='subject_list'),
-     path('subjects/<pk>/', views.SubjectDetailView.as_view(),
-          name='subject_detail'),
+     # path('subjects/', views.SubjectListView.as_view(), 
+     #      name='subject_list'),
+     # path('subjects/<pk>/', views.SubjectDetailView.as_view(),
+     #      name='subject_detail'),
+
      # ...:8000/api
      path('', include(router.urls)),
 
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -5,18 +5,24 @@ from courses.api.serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
 
 
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
-    pagination_class = StandardPagination
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
     pagination_class = StandardPagination
