c5f2a490a87516e5e1d5aadc0783efaccb90b0df
DeMonA1 <demonagrail1@gmail.com>
2024-11-21 08:40:10 +0300

    add contents action (CourseWithContentsSerializer)

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -6,8 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
+from courses.api.permissions import IsEnrolled
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
 from courses.models import Subject, Course
 
 
@@ -30,12 +31,21 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     @action(detail=True,        # this is an action to be performed on a single object
             methods=['post'],
             authentication_classes=[BasicAuthentication],
-            permission_classes=[IsAuthenticated])
-    
+            permission_classes=[IsAuthenticated]
+    )
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
