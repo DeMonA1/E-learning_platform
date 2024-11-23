d4d3e9953580d4606d4cd33a6f762da5013d3e40
DeMonA1 <demonagrail1@gmail.com>
2024-11-23 12:23:41 +0300

    routing for ChatConsumer

---
+++ educa/chat/routing.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from . import consumers
+
+
+websocket_urlpatterns = [
+    # pattern course_id any digits one or more in the end
+    re_path(r'ws/chat/room/(?P<course_id>\d+)$', 
+            consumers.ChatConsumer.as_asgi()),
+]
