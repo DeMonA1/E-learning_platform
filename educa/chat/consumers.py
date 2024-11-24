15cef56ee1b1a0f53fd19e0e12ad665a46502dfd
DeMonA1 <demonagrail1@gmail.com>
2024-11-24 07:26:48 +0300

    adding channel_layer and channel_group to Consumer

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,4 +1,5 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -9,15 +10,36 @@ class ChatConsumer(WebsocketConsumer):
     (like view in WSGI)
     """
     def connect(self):
+        # every consumer has a scope
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # join room group; use asy...sync() wrapper to use the channel
+        # layer async method in sync code
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
         # accept connection
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     # receive message from WebSocket
     def receive(self, text_data):
         text_data_json: dict = json.loads(text_data)
         message = text_data_json['message']
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            # type - method that should be invoked on consumers that
+            # receives the event
+            self.room_group_name, {'type': 'chat_message',
+                                   'message': message}
+        )
+        
+    # receive message from room group
+    def chat_message(self, event):
         # send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        self.send(text_data=json.dumps(event))
