9fde774af9f0044b957da57284640030b495aa12
DeMonA1 <demonagrail1@gmail.com>
2024-11-24 13:25:01 +0300

    display 5 latest_messages

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -8,7 +8,8 @@ from courses.models import Course
 @login_required
 def course_chat_room(request, course_id):
     """
-    Access to course chat-room only for enrolled users
+    Access to course chat-room only for enrolled users.
+    Return Course object and 5 Message objects
     """
     try:
         # retrieve course with given id joined by the current user
@@ -16,8 +17,14 @@ def course_chat_room(request, course_id):
     except Course.DoesNotExist:
         # user is not a student of the course or course does not exist
         return HttpResponseForbidden()  # 403
+    # retreive chat history; select_related - optimize db queiries
+    latest_messages = course.chat_messages.select_related(
+        'user'
+        ).order_by('-id')[:5]
+    latest_messages = reversed(latest_messages)
     return render(request, 
                   'chat/room.html', 
-                  {'course': course})
+                  {'course': course,
+                   'latest_messages': latest_messages})
 
 
